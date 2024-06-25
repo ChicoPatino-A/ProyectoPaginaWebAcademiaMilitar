1a2f6fb35b48a73b4864b2d50c8ffab0b90360d5
Johanaproyecto <johanachico@hotmail.com>
2024-06-25 18:28:38 +0200

    modificacion de diseño, creacion de la ruta login

--- app.py
+++ app.py
@@ -1,31 +1,66 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+import mysql.connector
 
 app = Flask(__name__)
 
-@app.route('/') # La ruta base. 
+# Configuración de la base de datos
+db_config = {
+    'user': 'root',          # Reemplaza con tu usuario de la base de datos
+    'password': '',     # Reemplaza con tu contraseña de la base de datos
+    'host': 'localhost',           # Reemplaza si tu host es diferente
+    'database': 'basededatosacademiamilitar' # Nombre de tu base de datos
+}
+
+@app.route('/')
 def home():
     return '¡Bienvenida, estoy en Casa!'
 
-@app.route('/johana') # La ruta base. 
+@app.route('/johana')
 def johana():
     return '¡Bienvenida Johana!'
 
-@app.route('/principal') # nombre de la ruta
-def principal(): # funcion que habre la ruta
-    return render_template('index.html') # render template -> abrir la plantilla (colocamos la plantilla que queremos abrir)
+@app.route('/principal')
+def principal():
+    return render_template('index.html')
 
-@app.route('/contacto') # nombre de la ruta
-def contacto(): # funcion que habre la ruta
+@app.route('/contacto')
+def contacto():
     return render_template('contacto.html')
 
-@app.route('/acercade') # nombre de la ruta
-def acercade(): # funcion que habre la ruta
+@app.route('/acercade')
+def acercade():
     return render_template('acercade.html')
 
-@app.route('/iniciarsesion') # nombre de la ruta
-def iniciarsesion(): # funcion que habre la ruta
+@app.route('/iniciarsesion')
+def iniciarsesion():
     return render_template('iniciarsesion.html')
+
+@app.route('/promociones')
+def promociones():
+    return render_template('promociones.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    # Guardo la informacion del formulario en las variables correo y contrasena
+    correo = request.form['correo']
+    contrasena = request.form['contrasena']
+
+    # Conectar a la base de datos
+    conn = mysql.connector.connect(**db_config)
+    cursor = conn.cursor()
+
+    # Consultar la base de datos
+    query = "SELECT * FROM usuarios WHERE email=%s AND password=%s"
+    cursor.execute(query, (correo, contrasena))
+    user = cursor.fetchone() # si encuentra al menos un usuario lo guarda en la varaible "user"
+    # si el usuario no existe usuario vale "NULL".
+    cursor.close()
+    conn.close()
+
+    if user: # preguntando si user EXISTE. 
+        return "Sí, hemos encontrado este usuario."
+    else:
+        return "Este usuario no existe."
 
 if __name__ == '__main__':
     app.run(debug=True, host='192.168.43.69', port=5000)
-
