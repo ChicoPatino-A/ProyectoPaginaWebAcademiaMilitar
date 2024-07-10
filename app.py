24dcf0e2a9b59ca366e6adfcd949345bc85947b0
Johanaproyecto <johanachico@hotmail.com>
2024-07-10 18:33:41 +0200

    agregar rutas para imagenes y video, conectar con base de datos para cargar imagenes y videos

--- app.py
+++ app.py
@@ -127,13 +127,90 @@ def guardarPromocion():
     else:
         return "Error al subir la imagen", 500
     
+
+@app.route('/guardarImagen', methods=['POST'])
+def guardarImagen():
+    titulo = request.form['tituloImagen']
+    resena = request.form['resenaImagen']
+    numeroDePromocion = request.form['numeroDePromocion']
+    UPLOAD_FOLDER = 'static/images' 
+    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+    json_key_path = 'C://Users//aclog//Desktop//ProyectoFinal//Codigo//academiadelogistica-4a432-firebase-adminsdk-j0rgu-442fbffdb6.json'
+    storage_bucket = 'academiadelogistica-4a432.appspot.com'
+
+    initialize_firebase(json_key_path, storage_bucket)
+
+    if 'imagenCargada' not in request.files:
+        return "No se encontró la imagen", 400
+
+    file = request.files['imagenCargada']
+    if file.filename == '':
+        return "No se seleccionó ninguna imagen", 400
+
+    if file:
+        filename = secure_filename(file.filename)
+        local_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(local_path)
+        
+        url_imagen = subir_imagen(local_path)
+        id_promocion = base_datos.obtener_id_promocion_por_numero(numeroDePromocion)
+        base_datos.guardar_imagen(titulo, resena,url_imagen,id_promocion)
+
+        os.remove(local_path)
+        return render_template('administracion.html', aviso = "¡La imagen se subio exitosamente!")
+    else:
+        return "Error al subir la imagen", 500
+
+def subir_video(local_path):
+    bucket = storage.bucket()
+    blob = bucket.blob(os.path.basename(local_path))
+    blob.upload_from_filename(local_path)
+    return blob.public_url
+
+@app.route('/guardarVideo', methods=['POST'])
+def guardarVideo():
+    titulo = request.form['tituloVideo']
+    resena = request.form['resenaVideo']
+    numeroDePromocion = request.form['numeroDePromocion']
+    UPLOAD_FOLDER = 'static/videos' 
+    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+    json_key_path = 'C://Users//aclog//Desktop//ProyectoFinal//Codigo//academiadelogistica-4a432-firebase-adminsdk-j0rgu-442fbffdb6.json'
+    storage_bucket = 'academiadelogistica-4a432.appspot.com'
+
+    initialize_firebase(json_key_path, storage_bucket)
+
+    if 'videoCargado' not in request.files:
+        return "No se encontró el video", 400
+
+    file = request.files['videoCargado']
+    if file.filename == '':
+        return "No se seleccionó ningún video", 400
+
+    if file:
+        filename = secure_filename(file.filename)
+        local_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(local_path)
+        
+        url_video = subir_video(local_path)
+        id_promocion = base_datos.obtener_id_promocion_por_numero(numeroDePromocion)
+        base_datos.guardar_video(titulo, resena, url_video, id_promocion)
+
+        os.remove(local_path)
+        return render_template('administracion.html', aviso = "¡El video se subió exitosamente!")
+    else:
+        return "Error al subir el video", 500
+
 @app.route('/promociones/<int:id>')
 def mostrar_promocion(id):
     promocion = base_datos.obtener_promocion_por_id(id)
+    imagenes = base_datos.obtener_imagenes_por_id_promocion(id)
+    print(promocion)
+    print("Imagenes = ", imagenes)
     if promocion:
-        return render_template('promocion_detalle.html', promocion=promocion)
+        return render_template('promocion_detalle.html', promocion=promocion, imagenes=imagenes)
     else:
         return "Promoción no encontrada.", 404
 
 if __name__ == '__main__':
-    app.run(debug=True, host='192.168.43.69', port=5000)
+    app.run(debug=True, host='10.108.4.35', port=5000)
+
--- modelos/BaseDeDatos.py
+++ modelos/BaseDeDatos.py
@@ -18,6 +18,30 @@ class BaseDeDatos:
         cursor.close()
         conn.close()
         return promociones
+    
+    def obtener_imagenes_por_id_promocion(self, id):
+        conn = self.conectar()
+        cursor = conn.cursor(dictionary=True)
+        
+        query = "SELECT titulo, comentario, url_imagen FROM imagenes WHERE id_promocion = %s"
+        cursor.execute(query,(id,))
+        imagenes_de_la_promocion = cursor.fetchall()
+        print(imagenes_de_la_promocion)
+        cursor.close()
+        conn.close()
+        return imagenes_de_la_promocion
+    
+    def obtener_videos_por_id_promocion(self, id):
+        conn = self.conectar()
+        cursor = conn.cursor(dictionary=True)
+        
+        query = "SELECT titulo, comentario, url_video FROM videos WHERE id_promocion = %s"
+        cursor.execute(query,(id,))
+        videos_de_la_promocion = cursor.fetchall()
+        print(videos_de_la_promocion)
+        cursor.close()
+        conn.close()
+        return videos_de_la_promocion
 
     def obtener_usuario(self, correo, contrasena):
         conn = self.conectar()
@@ -53,12 +77,46 @@ class BaseDeDatos:
         cursor.close()
         conn.close()
 
+    def obtener_id_promocion_por_numero(self,numeroDePromocion):
+            conn = self.conectar()
+            cursor = conn.cursor()
+            query = "SELECT id FROM promociones WHERE numero_promocion = %s"
+            cursor.execute(query, (numeroDePromocion,))
+            id = cursor.fetchone()
+            print(id)
+            return id
+    
+
+    def guardar_imagen(self,titulo,comentario, url_imagen, tupla_promocion ):
+            id_promocion = tupla_promocion[0]
+            conn = self.conectar()
+            cursor = conn.cursor()
+
+            query = "INSERT INTO imagenes (titulo,comentario, url_imagen, id_promocion) VALUES (%s, %s,%s, %s);"
+            cursor.execute(query, (titulo,comentario, url_imagen, id_promocion,))
+
+            conn.commit()
+            cursor.close()
+            conn.close()
+    def guardar_video(self,titulo,comentario, url_video, tupla_promocion ):
+            id_promocion = tupla_promocion[0]
+            conn = self.conectar()
+            cursor = conn.cursor()
+
+            query = "INSERT INTO videos (titulo,comentario, url_video, id_promocion) VALUES (%s, %s,%s, %s);"
+            cursor.execute(query, (titulo,comentario, url_video, id_promocion,))
+
+            conn.commit()
+            cursor.close()
+            conn.close()
+
     def obtener_promocion_por_id(self, id):
         conn = self.conectar()
         cursor = conn.cursor()
         query = "SELECT * FROM promociones WHERE id = %s"
         cursor.execute(query, (id,))
         promocion = cursor.fetchone()
+        print(promocion)
         return promocion
 
 
