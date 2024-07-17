f0e4b665f26c55f0798ce2f21ea94fd53e7ba998
Johanaproyecto <johanachico@hotmail.com>
2024-07-17 15:37:58 +0200

    añadir botones eliminar y editar en Administracion y crear aviso si usuario no existe

--- app.py
+++ app.py
@@ -1,5 +1,21 @@
-# app.py
+"""
+# Modulos
+# Creacion de la app con flask
+# Configuración de la base de datos
+# Inicialización de la base de datos
+# Inicializa Firebase
+# Rutas Basicas del NavBar
+# Ruta para crear Cuenta
+# Ruta para Hacer Login
+# Ruta para crear administrador
+# Rutas para promociones
+# Funcion para subir Video
+# Rutas para videos
+# Funcion para subir Imagen
+# Rutas para imagenes
+"""
 
+# Modulos
 from flask import Flask, render_template, request
 import os
 from werkzeug.utils import secure_filename
@@ -7,6 +23,7 @@ from modelos.BaseDeDatos import BaseDeDatos
 import firebase_admin
 from firebase_admin import credentials, storage
 
+# Creacion de la app con flask
 app = Flask(__name__)
 
 # Configuración de la base de datos
@@ -20,6 +37,15 @@ db_config = {
 # Inicialización de la base de datos
 base_datos = BaseDeDatos(db_config)
 
+
+# Inicializa Firebase
+def initialize_firebase(json_key_path, storage_bucket):
+    if not firebase_admin._apps:
+        cred = credentials.Certificate(json_key_path)
+        firebase_admin.initialize_app(cred, {'storageBucket': storage_bucket})
+
+# Rutas Basicas del NavBar
+
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -28,10 +54,10 @@ def home():
 def principal():
     return render_template('index.html')
 
-@app.route('/contacto')
-def contacto():
-    return render_template('contacto.html')
-
+@app.route('/contacto') # Ruta que obtiene la palabra /contacto
+def contacto(): # abre la funcion contacto
+    return render_template('contacto.html') # se dirige automaticamente a contacto.html
+ 
 @app.route('/acercade')
 def acercade():
     return render_template('acercade.html')
@@ -45,35 +71,8 @@ def promociones():
     promociones = base_datos.obtener_promociones()
     return render_template('promociones.html', promociones=promociones)
 
-@app.route('/login', methods=['POST'])
-def login():
-    correo = request.form['correo']
-    contrasena = request.form['contrasena']
 
-    user = base_datos.obtener_usuario(correo, contrasena)
-
-    if user:
-        return render_template('administracion.html')
-    else:
-        return "Este usuario no existe."
-
-@app.route('/crearPromocion')
-def crearPromocion():
-    return render_template('crearpromocion.html')
-
-@app.route('/crearAdministrador')
-def crearAdministrador():
-    return render_template('crearadministrador.html')
-
-@app.route('/subirVideo')
-def subirVideo():
-    promociones = base_datos.obtener_promociones()
-    return render_template('subirVideo.html',promociones = promociones)
-
-@app.route('/subirImagen')
-def subirImagen():
-    promociones = base_datos.obtener_promociones()
-    return render_template('subirImagen.html', promociones = promociones)
+# Ruta para crear Cuenta
 
 @app.route('/crearCuenta', methods=['POST'])
 def crearCuenta():
@@ -83,19 +82,47 @@ def crearCuenta():
     base_datos.crear_usuario(correo, contrasena)
     return render_template('administracion.html', aviso = "¡El usuario se cargo exitosamente!")
 
-# Inicializa Firebase
-def initialize_firebase(json_key_path, storage_bucket):
-    if not firebase_admin._apps:
-        cred = credentials.Certificate(json_key_path)
-        firebase_admin.initialize_app(cred, {'storageBucket': storage_bucket})
+# Ruta para Hacer Login
 
-# Función para subir imagen a Firebase
-def subir_imagen(file_path):
-    bucket = storage.bucket()
-    blob = bucket.blob(os.path.basename(file_path))
-    blob.upload_from_filename(file_path)
-    blob.make_public()
-    return blob.public_url
+@app.route('/login', methods=['POST'])
+def login():
+    correo = request.form['correo'] # obtiene el correo del formulario
+    contrasena = request.form['contrasena'] # obtiene la contraseña del formulario
+    # el usuario se obtiene de la BD por medio de una funcion "obtener_usuario" donde los
+    # parametros son: correo y contraseña 
+    usuario = base_datos.obtener_usuario(correo, contrasena)
+
+    if usuario:  # si el usuario existe
+        return render_template('administracion.html') # viajo a la plantilla administracion.html
+    else: # caso contrario voy a iniciarsesion, junto con un aviso
+
+        return render_template('iniciarsesion.html',  aviso = "¡El usuario no existe")
+
+# Ruta para crear administrador
+
+@app.route('/crearAdministrador')
+def crearAdministrador():
+    return render_template('crearadministrador.html')
+
+# Ruta para eliminar administrador
+
+@app.route('/eliminarAdministrador')
+def eliminarAdministrador():
+    return render_template('eliminarAdministrador.html')
+
+
+# Ruta para editar administrador
+
+@app.route('/editarAdministrador')
+def editarAdministrador():
+    return render_template('editarAdministrador.html')
+
+#  Rutas para Promociones
+# 1 - guardarPromocion
+# 2 - crearPromocion
+# 3 - promociones/{id} -> para acceder a alguna promocion
+# 4 - eliminarPromocion
+# 5 - editarPromocion
 
 @app.route('/guardarPromocion', methods=['POST'])
 def guardarPromocion():
@@ -127,47 +154,48 @@ def guardarPromocion():
         return render_template('administracion.html', aviso = "¡La promocion se cargo exitosamente!")
     else:
         return "Error al subir la imagen", 500
+ 
+@app.route('/crearPromocion')
+def crearPromocion():
+    return render_template('crearpromocion.html')
+
+@app.route('/promociones/<int:id>')
+def mostrar_promocion(id):
+    promocion = base_datos.obtener_promocion_por_id(id)
+    imagenes = base_datos.obtener_imagenes_por_id_promocion(id)
+    videos = base_datos.obtener_videos_por_id_promocion(id)
     
-
-@app.route('/guardarImagen', methods=['POST'])
-def guardarImagen():
-    titulo = request.form['tituloImagen']
-    resena = request.form['resenaImagen']
-    numeroDePromocion = request.form['numeroDePromocion']
-    UPLOAD_FOLDER = 'static/images' 
-    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-    json_key_path = 'C://Users//aclog//Desktop//ProyectoFinal//Codigo//academiadelogistica-4a432-firebase-adminsdk-j0rgu-442fbffdb6.json'
-    storage_bucket = 'academiadelogistica-4a432.appspot.com'
-
-    initialize_firebase(json_key_path, storage_bucket)
-
-    if 'imagenCargada' not in request.files:
-        return "No se encontró la imagen", 400
-
-    file = request.files['imagenCargada']
-    if file.filename == '':
-        return "No se seleccionó ninguna imagen", 400
-
-    if file:
-        filename = secure_filename(file.filename)
-        local_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-        file.save(local_path)
-        
-        url_imagen = subir_imagen(local_path)
-        id_promocion = base_datos.obtener_id_promocion_por_numero(numeroDePromocion)
-        base_datos.guardar_imagen(titulo, resena,url_imagen,id_promocion)
-
-        os.remove(local_path)
-        return render_template('administracion.html', aviso = "¡La imagen se subio exitosamente!")
+    if promocion:
+        return render_template('promocion_detalle.html', promocion=promocion, imagenes=imagenes, videos = videos)
     else:
-        return "Error al subir la imagen", 500
+        return "Promoción no encontrada.", 404
+    
+@app.route('/eliminarPromocion')
+def eliminarPromocion():
+    return render_template('eliminarPromocion.html')
 
+@app.route('/editarPromocion')
+def editarPromocion():
+    return render_template('editarPromocion.html')
+
+# Funcion para subir Video:
 def subir_video(local_path):
     bucket = storage.bucket()
     blob = bucket.blob(os.path.basename(local_path))
     blob.upload_from_filename(local_path)
     blob.make_public()
     return blob.public_url
+
+# Rutas para Videos:
+# 1 - Subir Video
+# 2 - Guardar Video
+# 3 - Eliminar Video
+# 4 - Editar Video
+
+@app.route('/subirVideo')
+def subirVideo():
+    promociones = base_datos.obtener_promociones()
+    return render_template('subirVideo.html',promociones = promociones)
 
 @app.route('/guardarVideo', methods=['POST'])
 def guardarVideo():
@@ -203,17 +231,79 @@ def guardarVideo():
     else:
         return "Error al subir el video", 500
 
-@app.route('/promociones/<int:id>')
-def mostrar_promocion(id):
-    promocion = base_datos.obtener_promocion_por_id(id)
-    imagenes = base_datos.obtener_imagenes_por_id_promocion(id)
-    videos = base_datos.obtener_videos_por_id_promocion(id)
-    
-    if promocion:
-        return render_template('promocion_detalle.html', promocion=promocion, imagenes=imagenes, videos = videos)
-    else:
-        return "Promoción no encontrada.", 404
+@app.route('/eliminarVideo')
+def eliminarVideo():
+    return render_template('eliminarVideo.html')
 
+@app.route('/editarVideo')
+def editarVideo():
+    return render_template('editarVideo.html')
+
+# Funcion para subir imagen
+
+# Función para subir imagen a Firebase
+def subir_imagen(file_path):
+    bucket = storage.bucket()
+    blob = bucket.blob(os.path.basename(file_path))
+    blob.upload_from_filename(file_path)
+    blob.make_public()
+    return blob.public_url
+
+
+# Rutas para las imagenes: 
+# 1 - Subir Imagen
+# 2 - Guardar Imagen
+# 3 - Eliminar Imagen
+# 4 - Editar Imagen
+
+@app.route('/subirImagen')
+def subirImagen():
+    promociones = base_datos.obtener_promociones()
+    return render_template('subirImagen.html', promociones = promociones)
+
+@app.route('/guardarImagen', methods=['POST'])
+def guardarImagen():
+    titulo = request.form['tituloImagen']
+    resena = request.form['resenaImagen']
+    numeroDePromocion = request.form['numeroDePromocion']
+    UPLOAD_FOLDER = 'static/images' 
+    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+    json_key_path = 'C://Users//aclog//Desktop//ProyectoFinal//Codigo//academiadelogistica-4a432-firebase-adminsdk-j0rgu-442fbffdb6.json'
+    storage_bucket = 'academiadelogistica-4a432.appspot.com'
+
+    initialize_firebase(json_key_path, storage_bucket)
+
+    if 'imagenCargada' not in request.files:
+        return "No se encontró la imagen", 400
+
+    file = request.files['imagenCargada']
+    if file.filename == '':
+        return "No se seleccionó ninguna imagen", 400
+
+    if file:
+        filename = secure_filename(file.filename)
+        local_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(local_path)
+        
+        url_imagen = subir_imagen(local_path)
+        id_promocion = base_datos.obtener_id_promocion_por_numero(numeroDePromocion)
+        base_datos.guardar_imagen(titulo, resena,url_imagen,id_promocion)
+
+        os.remove(local_path)
+        return render_template('administracion.html', aviso = "¡La imagen se subio exitosamente!")
+    else:
+        return "Error al subir la imagen", 500
+
+@app.route('/eliminarImagen')
+def eliminarImagen():
+    return render_template('eliminarImagen.html')
+
+@app.route('/editarImagen')
+def editarImagen():
+    return render_template('editarImagen.html')
+
+
+# Funcion main de arranque del programa
 if __name__ == '__main__':
     app.run(debug=True, host='192.168.43.69', port=5000)
 
--- modelos/BaseDeDatos.py
+++ modelos/BaseDeDatos.py
@@ -1,5 +1,25 @@
 import mysql.connector
+"""
+# Logica para PROMOCIONES
+obtener_promociones
+guardar_promocion
+obtener_id_promocion_por_numero
+obtener_promocion_por_id
 
+# Logica para USUARIOS
+obtener_usuario
+crear_usuario
+
+# Logica para IMAGENES
+
+guardar_imagen
+obtener_imagenes_por_id_promocion
+
+# Logica para VIDEOS
+guardar_video
+obtener_videos_por_id_promocion
+
+"""
 class BaseDeDatos:
     def __init__(self, config):
         self.config = config
@@ -7,6 +27,7 @@ class BaseDeDatos:
     def conectar(self):
         return mysql.connector.connect(**self.config)
     
+    # Logica para PROMOCIONES
     def obtener_promociones(self):
         conn = self.conectar() 
         cursor = conn.cursor(dictionary=True)
@@ -19,52 +40,6 @@ class BaseDeDatos:
         conn.close()
         return promociones
     
-    def obtener_imagenes_por_id_promocion(self, id):
-        conn = self.conectar()
-        cursor = conn.cursor(dictionary=True)
-        
-        query = "SELECT titulo, comentario, url_imagen FROM imagenes WHERE id_promocion = %s"
-        cursor.execute(query,(id,))
-        imagenes_de_la_promocion = cursor.fetchall()
-        print(imagenes_de_la_promocion)
-        cursor.close()
-        conn.close()
-        return imagenes_de_la_promocion
-    
-    def obtener_videos_por_id_promocion(self, id):
-        conn = self.conectar()
-        cursor = conn.cursor(dictionary=True)
-        
-        query = "SELECT titulo, comentario, url_video FROM videos WHERE id_promocion = %s"
-        cursor.execute(query,(id,))
-        videos_de_la_promocion = cursor.fetchall()
-        print(videos_de_la_promocion)
-        cursor.close()
-        conn.close()
-        return videos_de_la_promocion
-
-    def obtener_usuario(self, correo, contrasena):
-        conn = self.conectar()
-        cursor = conn.cursor()
-
-        query = "SELECT * FROM usuarios WHERE email=%s AND password=%s"
-        cursor.execute(query, (correo, contrasena))
-        user = cursor.fetchone()
-
-        cursor.close()
-        conn.close()
-        return user
-
-    def crear_usuario(self, correo, contrasena):
-        conn = self.conectar()
-        cursor = conn.cursor()
-
-        query = "INSERT INTO usuarios(email, password) VALUES (%s, %s);"
-        cursor.execute(query, (correo, contrasena))
-
-        conn.commit()
-        cursor.close()
-        conn.close()
 
     def guardar_promocion(self, numero_promocion, url_imagen):
         conn = self.conectar()
@@ -77,6 +52,7 @@ class BaseDeDatos:
         cursor.close()
         conn.close()
 
+    
     def obtener_id_promocion_por_numero(self,numeroDePromocion):
             conn = self.conectar()
             cursor = conn.cursor()
@@ -86,31 +62,6 @@ class BaseDeDatos:
             print(id)
             return id
     
-
-    def guardar_imagen(self,titulo,comentario, url_imagen, tupla_promocion ):
-            id_promocion = tupla_promocion[0]
-            conn = self.conectar()
-            cursor = conn.cursor()
-
-            query = "INSERT INTO imagenes (titulo,comentario, url_imagen, id_promocion) VALUES (%s, %s,%s, %s);"
-            cursor.execute(query, (titulo,comentario, url_imagen, id_promocion,))
-
-            conn.commit()
-            cursor.close()
-            conn.close()
-    def guardar_video(self,titulo,comentario, url_video, tupla_promocion ):
-            print("Tupla_promocion es:", tupla_promocion)
-            id_promocion = tupla_promocion[0]
-            conn = self.conectar()
-            cursor = conn.cursor()
-
-            query = "INSERT INTO videos (titulo,comentario, url_video, id_promocion) VALUES (%s, %s,%s, %s);"
-            cursor.execute(query, (titulo,comentario, url_video, id_promocion,))
-
-            conn.commit()
-            cursor.close()
-            conn.close()
-
     def obtener_promocion_por_id(self, id):
         conn = self.conectar()
         cursor = conn.cursor()
@@ -119,5 +70,84 @@ class BaseDeDatos:
         promocion = cursor.fetchone()
         print(promocion)
         return promocion
+    #FIN ----------------------------------------------------------------------Logica para PROMOCIONES
+    #Logica para USUARIOS
+    def obtener_usuario(self, correo, contrasena):
+        conn = self.conectar()
+        cursor = conn.cursor()
 
+        query = "SELECT * FROM usuarios WHERE email=%s AND password=%s"
+        cursor.execute(query, (correo, contrasena))
+        user = cursor.fetchone()
+
+        cursor.close()
+        conn.close()
+        return user
+    
+    def crear_usuario(self, correo, contrasena):
+        conn = self.conectar()
+        cursor = conn.cursor()
+
+        query = "INSERT INTO usuarios(email, password) VALUES (%s, %s);"
+        cursor.execute(query, (correo, contrasena))
+
+        conn.commit()
+        cursor.close()
+        conn.close()
+    #FIN ----------------------------------------------------------------------Logica para USUARIOS
+    # Logica para IMAGENES  
+    def guardar_imagen(self,titulo,comentario, url_imagen, tupla_promocion ):
+        id_promocion = tupla_promocion[0]
+        conn = self.conectar()
+        cursor = conn.cursor()
+
+        query = "INSERT INTO imagenes (titulo,comentario, url_imagen, id_promocion) VALUES (%s, %s,%s, %s);"
+        cursor.execute(query, (titulo,comentario, url_imagen, id_promocion,))
+
+        conn.commit()
+        cursor.close()
+        conn.close()
+
+    def obtener_imagenes_por_id_promocion(self, id):
+        conn = self.conectar()
+        cursor = conn.cursor(dictionary=True)
+        
+        query = "SELECT titulo, comentario, url_imagen FROM imagenes WHERE id_promocion = %s"
+        cursor.execute(query,(id,))
+        imagenes_de_la_promocion = cursor.fetchall()
+        print(imagenes_de_la_promocion)
+        cursor.close()
+        conn.close()
+        return imagenes_de_la_promocion
+
+    #FIN ----------------------------------------------------------------------Logica para IMAGENES
+
+     # Logica para VIDEOS  
+
+    def guardar_video(self,titulo,comentario, url_video, tupla_promocion ):
+        print("Tupla_promocion es:", tupla_promocion)
+        id_promocion = tupla_promocion[0]
+        conn = self.conectar()
+        cursor = conn.cursor()
+
+        query = "INSERT INTO videos (titulo,comentario, url_video, id_promocion) VALUES (%s, %s,%s, %s);"
+        cursor.execute(query, (titulo,comentario, url_video, id_promocion,))
+
+        conn.commit()
+        cursor.close()
+        conn.close()
+    def obtener_videos_por_id_promocion(self, id):
+        conn = self.conectar()
+        cursor = conn.cursor(dictionary=True)
+        
+        query = "SELECT titulo, comentario, url_video FROM videos WHERE id_promocion = %s"
+        cursor.execute(query,(id,))
+        videos_de_la_promocion = cursor.fetchall()
+        print(videos_de_la_promocion)
+        cursor.close()
+        conn.close()
+        return videos_de_la_promocion
+
+
+ #FIN ----------------------------------------------------------------------Logica para VIDEOS
 
